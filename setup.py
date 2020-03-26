8f8ae6ada6885cc45c260a79fcac535184e7ed83
Justin W Walthers <justin.walthers@hotmail.com>
2020-03-26 15:41:54 -0400

    update setup.py script to include required packages

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JayWaWa/caastools",
+    install_requires=['lxml >= 4.3.3', 'peewee >= 3.0', 'numpy >= 1.16.0', 'scipy >= 1.3.0',
+                      'pandas >= 1.0.0', 'matplotlib >= 3.1.0', 'seaborn >= 0.10.0', 'savReaderWriter >= 3.4.0'],
     packages=setuptools.find_packages(),
     package_data={'': ['*.xml', '*.xsd', '*.xslt']},
     include_package_data=True,
