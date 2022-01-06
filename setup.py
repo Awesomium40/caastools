8f5167b54b671faee10e2f5ebb4e182f2947a3d0
Justin W Walthers <justin.walthers@hotmail.com>
2022-01-06 06:06:27 -0500

    Update to 1.1.5.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
 
 setuptools.setup(
     name="caastools",
-    version="1.1.5.2",
+    version="1.1.5.3",
     author="Jay Walthers",
     author_email="justin_walthers@brown.edu",
     description="A package for managing and analyzing Interview data at CAAS",
