af6e9cc44db4fde190fd4c896931f602358f4d75
Justin W Walthers <justin.walthers@hotmail.com>
2022-01-16 18:22:55 -0500

    Add Skeleton of tests for Translator class

--- caastools/database/translation.py
+++ caastools/database/translation.py
@@ -1,7 +1,18 @@
 from caastools.database.model import *
-from caastools.database.database import atomic
+from caastools.database.database import atomic, db
 from peewee import PeeweeException
-from typing import Iterable, Type, Union
+from typing import Iterable, Type, Optional, Union
+
+
+__all__ = ['Translation', 'TranslationError', 'Translator']
+
+
+def copy_decorator(method):
+    def inner(self, *args, **kwargs):
+        clone = self.clone()
+        method(clone, *args, **kwargs)
+        return clone
+    return inner
 
 
 class TranslationError(Exception):
@@ -18,6 +29,71 @@ class TranslationError(Exception):
         self._source_system = source_system_name
         self._target_system = target_system_name
         self._source_pks = source_value_ids
+
+
+class Translator:
+
+    _query_params_ = {'global': (GlobalValue, 'GlobalValue', GlobalValue.global_value_id.name),
+                      'property': (PropertyValue, 'PropertyValue', PropertyValue.property_value_id.name)}
+
+    def __init__(self, source_cs_name, target_cs_name):
+        super().__init__()
+        self._source_system = CodingSystem.select().where(CodingSystem.cs_name == source_cs_name).get()
+        self._target_system = CodingSystem.select().where(CodingSystem.cs_name == target_cs_name).get()
+        self._target_pv = None
+        self._source_pvs = []
+        self._predicate = (
+                (Translation.source_cs == self._source_system) &
+                (Translation.target_cs == self._target_system)
+        )
+
+    def translate(self, translation_type, *sources) -> Optional[PropertyValue]:
+        """
+        Performs a translation of type translation_type from sources to their target
+        :param translation_type: type of translation to perform ('global' | 'property')
+        :param sources: iterable of PropertyValue objects from which a translation occurs
+        :return: PropertyValue that is the result of translation of None if not found
+        """
+        if len(sources) == 0:
+            raise ValueError("No sources provided for translation.")
+
+        predicate = self._predicate
+
+        parent_table, parent_table_name, id_attr = self._query_params_.get(translation_type, (None, None, None))
+        target_table = parent_table.alias('target')
+
+        if parent_table is None:
+            raise ValueError(f"parameter 'translation_type' expected ['global'|'property'], got {translation_type}")
+
+        target_cs_table: CodingSystem = CodingSystem.alias('target_system')
+        source_cs_table: CodingSystem = CodingSystem.alias('source_system')
+
+        # Construct the basic query - select the target value were source and target coding systems match specified
+        query = (
+            target_table
+            .select()
+            .join(TranslationTarget, on=target_table.property_value_id == TranslationTarget.parent_primary_key)
+            .join(Translation)
+            .join(target_cs_table, on=Translation.target_cs_id == target_cs_table.coding_system_id)
+            .switch(Translation)
+            .join(source_cs_table, on=Translation.source_cs == source_cs_table.coding_system_id)
+            .switch(Translation)
+        )
+
+        # Each source value represents part of the rule for translation
+        # Join the rule to the basic query and construct the predicate for the where clause
+        for i, pv in enumerate(sources):  # type: Union[Type[GlobalValue, PropertyValue]]
+            new_source: TranslationSource = TranslationSource.alias(f'source_{i}')
+            query = query.join(new_source).switch(Translation)
+            predicate = predicate & (
+                    (new_source.parent_table_name == parent_table_name) &
+                    (new_source.parent_primary_key == getattr(pv, id_attr))
+            )
+
+        result = query.where(predicate).first()
+
+        return result
+
 
 
 def _add_translation_(
